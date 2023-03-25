a846177f936f6610c1e4904e57cf0687814e3618
johnefitz <L00177804@atu.ie>
2023-03-25 12:50:34 +0000

    Added Reminder Model

---
+++ L00177804_Project/Models/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L00177804_Project.Models
+{
+    public class Messages
+    {
+        public string Message { get; set; }
+        public string MessageDate { get; set; }
+    }
+}
