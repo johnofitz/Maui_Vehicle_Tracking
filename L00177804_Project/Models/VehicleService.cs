717be8cee7e86cd7948d90e0fbdad97cbb4326e4
johnefitz <L00177804@atu.ie>
2023-03-25 12:40:14 +0000

    Added VehicleServiceModel

---
+++ L00177804_Project/Models/VehicleService.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace L00177804_Project.Models
+{
+    public class VehicleService
+    {
+        public string KmUntilService { get; set; }
+        public string ServiceDate { get; set; }
+    }
+}
