1686cc20731c94d903ee16ae58986f12775dfcbf
johnefitz <l00177804@atu.ie>
2023-04-20 14:35:11 +0100

    Add Vehicle Calculation Service Tests

--- L00177804_Project/Service/VehicleCalculationService/VehicleCalculations.cs
+++ L00177804_Project/Service/VehicleCalculationService/VehicleCalculations.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace L00177804_Project.Service.VehicleCalculationService
 {
-    internal class VehicleCalculations
+    public class VehicleCalculations
     {
+        private double co2Factor = 0.0; // Conversion factor for CO2 emissions
+
+        // Create constructor
+        public VehicleCalculations()
+        {
+        }
+
+        /// <summary>
+        ///  Calculate the fuel consumed over the total journey
+        /// </summary>
+        /// <param name="distance"></param>
+        /// <param name="fuel"></param>
+        /// <returns></returns>
+        public Task<double> CalculateFuelConsumption(double distance, double fuel)
+        {
+            double fuelConsumption = distance / fuel;
+
+            return Task.FromResult(fuelConsumption);
+        }
+
+        /// <summary>
+        /// Calculate the CO2 emissions based on the fuel consumption and average speed
+        /// </summary>
+        /// <param name="fuelConsumption"></param>
+        /// <param name="averageSpeed"></param>
+        /// <returns></returns>
+        public Task<double> CalculateCO2Emissions(double fuelConsumption, double averageSpeed, string fuelType)
+        {
+    
+            if (fuelType == "gasoline")
+            {
+                co2Factor = 2.31; // Conversion factor for gasoline in kg of CO2 per gallon
+            }
+            else if (fuelType == "diesel")
+            {
+                co2Factor = 2.68; // Conversion factor for diesel in kg of CO2 per liter
+            }
+
+            // Calculate CO2 emissions based on the fuel consumption and average speed
+            double co2Emissions = fuelConsumption * co2Factor * (averageSpeed / 60.0);
+
+            // Return the CO2 emissions as a double
+            return Task.FromResult(co2Emissions);
+        }
+
     }
 }
