e60e22d7ec578087dd30d13cfc16aca4ada964fd
johnefitz <l00177804@atu.ie>
2023-04-30 21:37:30 +0100

    Update on UI For most efficent route calculation

--- L00177804_Project/AppShell.xaml.cs
+++ L00177804_Project/AppShell.xaml.cs
@@ -6,8 +6,6 @@ namespace L00177804_Project;
 
 public partial class AppShell : Shell
 {
-   
-
     public AppShell()
     {
         InitializeComponent();
@@ -27,24 +25,20 @@ public partial class AppShell : Shell
     ShellContent _previousShellContent; // declare a variable to hold the previously viewed ShellContent
 
     protected override void OnNavigated(ShellNavigatedEventArgs args)
-    {
+    { 
+            // Check if the user has navigated to a new ShellContent and that there was a previously viewed ShellContent
+            if (CurrentItem?.CurrentItem?.CurrentItem is not null && _previousShellContent is not null)
+            {
+                // Get the ContentCache property of the previously viewed ShellContent using reflection
+                var property = typeof(ShellContent)
+                    .GetProperty("ContentCache", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+
+                // Set the value of the ContentCache property to null to clear the cache
+                property.SetValue(_previousShellContent, null);
+            }
         base.OnNavigated(args);
-
-        // Check if the user has navigated to a new ShellContent and that there was a previously viewed ShellContent
-        if (CurrentItem?.CurrentItem?.CurrentItem is not null && _previousShellContent is not null)
-        {
-            // Get the ContentCache property of the previously viewed ShellContent using reflection
-            var property = typeof(ShellContent)
-                .GetProperty("ContentCache", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
-
-            // Set the value of the ContentCache property to null to clear the cache
-            property.SetValue(_previousShellContent, null);
-        }
-
         // Set the _previousShellContent variable to the currently viewed ShellContent for future reference
         _previousShellContent = CurrentItem?.CurrentItem?.CurrentItem;
-
-
     }
 }
 
--- L00177804_Project/View/MainPage.xaml.cs
+++ L00177804_Project/View/MainPage.xaml.cs
@@ -4,31 +4,12 @@ using L00177804_Project.Service.VehicleInfoService;
 
 public partial class MainPage : ContentPage
 {
-    public MainPageViewModel ViewModel;
-    public MainPage()
+    
+    public MainPage(MainPageViewModel viewModel)
     {
         InitializeComponent();
-
-        ViewModel = new MainPageViewModel(new VehicleDataService());
-
-        BindingContext = ViewModel;
+        BindingContext = viewModel;
     }
-
-    protected override void OnNavigatedFrom(NavigatedFromEventArgs args)
-    {
-
-        base.OnNavigatedFrom(args);
-    }
-
-
-
-    protected override void OnNavigatedTo(NavigatedToEventArgs args)
-    {
-        // ViewModel = new MainPageViewModel(new Service.VehicleInfoService.VehicleDataService());
-        //     BindingContext = ViewModel;
-        //     base.OnNavigatedTo(args);
-    }
-
 
 }
 
