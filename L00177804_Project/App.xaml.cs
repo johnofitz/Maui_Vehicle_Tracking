1a1f14a64c72a5880096e1cfef50ce6961d11989
johnefitz <l00177804@atu.ie>
2023-05-11 13:25:47 +0100

    Added Secure Storage to Graph Api

--- L00177804_Project/App.xaml.cs
+++ L00177804_Project/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.Messaging;
+﻿
+using CommunityToolkit.Mvvm.Messaging;
 using L00177804_Project.Service.ThemeService;
 using L00177804_Project.Service.VehicleStoreService;
 
@@ -7,14 +8,15 @@ namespace L00177804_Project;
 
 public partial class App : Application
 {
-
-	public App()
+    private readonly string syncKey = "syncfusion";
+    public App()
 	{
-        
-    
+        string key = GetKey();
+ 
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(key);
+   
 		InitializeComponent();
 		MainPage = new AppShell();
-        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBaFt+QHFqVkNrWU5FdkBAXWFKblR8RWNTfVlgBShNYlxTR3ZbQ11jTn5Xd01jWnhd;Mgo+DSMBPh8sVXJ1S0d+X1RPc0BDXXxLflF1VWRTf156dlJWESFaRnZdQV1nSH5TcUVnWHdedXRW;ORg4AjUWIQA/Gnt2VFhhQlJBfVpdXGNWfFN0RnNedV5wfldBcC0sT3RfQF5jTX9TdkJjXX5WcHJWRQ==;MTY3MDU0MUAzMjMxMmUzMTJlMzMzNVZGNjY2UU1uT3lUZUhvc25wT1M3b0tGVlZ0a0phak9jVUw3OFJkeE1lbU09;MTY3MDU0MkAzMjMxMmUzMTJlMzMzNWY2VGNhR1prNlFJdWc0c3RnU0RITW03WDJwUGtiU3N5VTlGSElQbU0wYUE9;NRAiBiAaIQQuGjN/V0d+XU9Hc1RHQmJJYVF2R2BJeFRwdF9EZ0wgOX1dQl9gSXpSdkVkWXtfeHZWQ2M=;MTY3MDU0NEAzMjMxMmUzMTJlMzMzNW0zVTZSaVpSdFkyTm9FemQ2MGZpTys2MSt5dzNNN2h4T1h4bFpXQUxqUXc9;MTY3MDU0NUAzMjMxMmUzMTJlMzMzNWNMSys3bGU2c0c2cmZ2UHRTR013ODNscm0rKzd5RjI2REFHemMwaFlJUTQ9;Mgo+DSMBMAY9C3t2VFhhQlJBfVpdXGNWfFN0RnNedV5wfldBcC0sT3RfQF5jTX9TdkJjXX5WcnxRRQ==;MTY3MDU0N0AzMjMxMmUzMTJlMzMzNVJNeDdPSVJNNEZQS0FFN2NJdHBXNmxOSjFIYkVMNWZCRC9SckFRcXBNNnc9;MTY3MDU0OEAzMjMxMmUzMTJlMzMzNWxnNUlzcUY0eVZnemo1RE9YWWtsdVczNjZhT2srY1lXZk1ZZmxDUTZnTVU9;MTY3MDU0OUAzMjMxMmUzMTJlMzMzNW0zVTZSaVpSdFkyTm9FemQ2MGZpTys2MSt5dzNNN2h4T1h4bFpXQUxqUXc9");
 
         // Register the recipient with the messenger again
         WeakReferenceMessenger.Default.Register<VehicleChangedMessage>(this, LoadCarHandler);
@@ -26,6 +28,13 @@ public partial class App : Application
 
         LoadTheme(theme);
         LoadCar(cars);
+    }
+  
+    private string GetKey()
+    {
+       
+        string value = SecureStorage.GetAsync(syncKey).Result;
+        return value.ToString();
     }
 
     private void LoadTheme(string theme)
